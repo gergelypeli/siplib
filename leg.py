5c92ece21ebfb72d8b968631e5e9a18f41baf2cb
Gergely Peli <gergely.peli@ply.hu>
2015-07-14 00:13:20 +0200

    Leg invite state cleaned up a bit.

--- leg.py
+++ leg.py
@@ -20,6 +20,16 @@ class Leg(object):
         raise NotImplementedError()
 
 
+class InviteState(object):
+    PROVISIONAL_SESSION_NONE = "PROVISIONAL_SESSION_NONE"
+    PROVISIONAL_SESSION_UNRELIABLE = "PROVISIONAL_SESSION_UNRELIABLE"
+    PROVISIONAL_SESSION_RELIABLE = "PROVISIONAL_SESSION_RELIABLE"
+
+    def __init__(self, request):
+        self.request = request
+        self.provisional_session = self.PROVISIONAL_SESSION_NONE
+
+
 class SipLeg(Leg):
     DOWN = "DOWN"
     DIALING_IN = "DIALING_IN"
@@ -30,12 +40,15 @@ class SipLeg(Leg):
     UP = "UP"
     DISCONNECTING_OUT = "DISCONNECTING_OUT"
     
+
     def __init__(self, dialog):
         super(SipLeg, self).__init__()
         self.state = self.DOWN
         self.dialog = dialog
-        self.pending_received_message = None  # TODO: rethink these!
-        self.pending_sent_message = None
+        self.invite_state = None
+        #self.received_invite = None  # TODO: rethink these!
+        #self.sent_invite = None
+        #self.provisional_session = self.PROVISIONAL_SESSION_NONE
         
         self.dialog.set_report(WeakMethod(self.process))
 
@@ -44,15 +57,16 @@ class SipLeg(Leg):
         self.dialog.send_request(request, related)
 
 
-    def send_response(self, response):
-        if self.pending_received_message and not self.pending_received_message["is_response"]:
-            self.dialog.send_response(response, self.pending_received_message)
+    def send_response(self, response, related):
+        if related and not related["is_response"]:
+            self.dialog.send_response(response, related)
         else:
             raise Error("Respond to what?")
 
 
     def do(self, action):
         type = action["type"]
+        sdp = action.get("sdp")
         
         if self.state == self.DOWN:
             if type == "dial":
@@ -62,24 +76,25 @@ class SipLeg(Leg):
                     self.ctx.get("route"), self.ctx.get("hop")
                 )
                 
-                self.pending_sent_message = dict(method="INVITE", sdp=action.get("sdp"))
-                self.send_request(self.pending_sent_message)  # Will be extended!
+                invite = dict(method="INVITE", sdp=sdp)
+                self.send_request(invite)  # Will be extended!
+                self.invite_state = InviteState(invite)
                 self.state = self.DIALING_OUT
                 return
         elif self.state in (self.DIALING_OUT, self.DIALING_OUT_RINGING):
             if type == "hangup":
-                self.send_request(dict(method="CANCEL"), self.pending_sent_message)
+                self.send_request(dict(method="CANCEL"), self.invite_state.request)
                 self.state = self.DISCONNECTING_OUT
                 return
         elif self.state in (self.DIALING_IN, self.DIALING_IN_RINGING):
             if type == "ring":
                 if self.state == self.DIALING_IN:
-                    self.send_response(dict(status=Status(180, "Ringing")))
+                    self.send_response(dict(status=Status(180, "Ringing")), self.invite_state.request)
                     self.state = self.DIALING_IN_RINGING
                 return
             elif type == "answer":
-                self.send_response(dict(status=Status(200, "OK"), sdp=action.get("sdp")))
-                self.state = self.DIALING_IN_ANSWERED
+                self.send_response(dict(status=Status(200, "OK"), sdp=sdp), self.invite_state.request)
+                self.state = self.DIALING_IN_ANSWERED  # TODO: into InviteState?
                 # Must wait for the ACK
                 return
         elif self.state == self.UP:
@@ -95,6 +110,7 @@ class SipLeg(Leg):
         is_response = msg["is_response"]
         method = msg["method"]
         status = msg.get("status")
+        sdp = msg.get("sdp")
         
         if self.state == self.DOWN:
             if not is_response and method == "INVITE":
@@ -103,16 +119,16 @@ class SipLeg(Leg):
                     "to": msg["to"]
                 })
                 
-                self.pending_received_message = msg
-                self.report(dict(type="dial", ctx=self.ctx, sdp=msg.get("sdp")))
+                self.invite_state = InviteState(msg)
+                self.report(dict(type="dial", ctx=self.ctx, sdp=sdp))
                 self.state = self.DIALING_IN
                 return
         elif self.state in (self.DIALING_IN, self.DIALING_IN_RINGING):
             if not is_response and method == "CANCEL":
                 self.report(dict(type="hangup"))
-                self.send_response(dict(status=Status(487, "Request Terminated")))  # for the INVITE
-                self.pending_received_message = msg
-                self.send_response(dict(status=Status(200, "OK")))  # for the CANCEL
+                self.send_response(dict(status=Status(487, "Request Terminated")), self.invite_state.request)
+                self.send_response(dict(status=Status(200, "OK")), msg)
+                self.invite_state = None
                 self.state = self.DOWN
                 return
         elif self.state in (self.DIALING_OUT, self.DIALING_OUT_RINGING):
@@ -128,27 +144,27 @@ class SipLeg(Leg):
                     # ACKed by tr
                     return
                 elif status.code >= 200:
-                    self.report(dict(type="answer", sdp=msg.get("sdp")))
+                    self.report(dict(type="answer", sdp=sdp))
                     self.state = self.UP
                     self.send_request(dict(method="ACK"), msg)
                     return
         elif self.state == self.DIALING_IN_ANSWERED:
             if not is_response and method == "ACK":
-                self.send_response(make_virtual_response())  # for the INVITE
-                self.pending_received_message = msg
-                self.send_response(make_virtual_response())  # for the ACK
-                self.pending_received_message = None
+                # Stop the retransmission of the final answer
+                self.send_response(make_virtual_response(), self.invite_state.request)
+                # Let the ACK server transaction expire
+                self.send_response(make_virtual_response(), msg)
+                self.invite_state = None
                 self.state = self.UP
                 return
-            elif not is_response and method == "NAK":  # virtual request
+            elif not is_response and method == "NAK":  # virtual request, no ACK received
                 self.send_request(dict(method="BYE"))  # required behavior
                 self.state = self.DISCONNECTING_OUT
                 return
         elif self.state == self.UP:
             if not is_response and method == "BYE":
-                self.pending_received_message = msg
                 self.report(dict(type="hangup"))
-                self.send_response(dict(status=Status(200, "OK")))
+                self.send_response(dict(status=Status(200, "OK")), msg)
                 self.state = self.DOWN
                 return
         elif self.state == self.DISCONNECTING_OUT:
@@ -163,7 +179,6 @@ class SipLeg(Leg):
             elif is_response and method == "CANCEL":
                 print("Got cancel response: %s" % (status,))
                 return
-                
                 
         raise Error("Weirdness!")
 
